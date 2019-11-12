c0df52cce0044924653b9bf5c15cdeda48936627
AleksandrUzhegov <alexug@narod.ru>
2019-11-12 20:00:29 +0500

    Задание 7

---
+++ lesson_7/src/main/java/com/ertelecom/lesson_7/Car.java
@@ -0,0 +1,53 @@
+package com.ertelecom.lesson_7;
+
+import java.util.concurrent.BrokenBarrierException;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0 ;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+
+    public String getName () {
+        return name;
+    }
+    public int getSpeed () {
+        return speed;
+    }
+    public Car (Race race, int speed) {
+        this .race = race;
+        this .speed = speed;
+        CARS_COUNT++;
+        this .name = "Участник #" + CARS_COUNT;
+    }
+
+    @Override
+    public void run () {
+        try {
+            System.out.println( this .name + " готовится" );
+            Thread.sleep( 500 + ( int )(Math.random() * 800 ));
+            System.out.println( this .name + " готов" );
+            MainApp.cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        for ( int i = 0 ; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go( this );
+        }
+
+        // финиш
+        try {
+            MainApp.finish.lock();
+            if (MainApp.countDown.getCount() == MainApp.CARS_COUNT) {
+                System.out.println(this.name + " ПОБЕДИТЕЛЬ");
+            }
+            MainApp.countDown.countDown();
+        } finally {
+            MainApp.finish.unlock();
+        }
+    }
+}
