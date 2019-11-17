df06ff949b8c8e2e8fb62bde2360abd6ef751588
AleksandrUzhegov <alexug@narod.ru>
2019-11-17 20:26:31 +0500

    Задание 9

---
+++ lesson_9/src/main/java/com/ertelecom/lesson_9/Task.java
@@ -0,0 +1,73 @@
+package com.ertelecom.lesson_9;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    public enum Status implements Serializable {
+        CREATED("Создана",1), CLOSSED("Закрыта",3), REJECTED("Отклонена",2);
+        private String rusTitle;
+        private int order;
+
+        public String getRusTitle() {
+            return rusTitle;
+        }
+
+        Status(String rusTitle, int order) {
+            this.rusTitle = rusTitle;
+            this.order = order;
+        }
+        }
+
+    private long id;
+    private String taskName;
+    private String ownerName;
+    private String executorName;
+    private String info;
+    private Status status;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Task(long id, String taskName, String ownerName, String executorName, String info) {
+        this.id = id;
+        this.taskName = taskName;
+        this.ownerName = ownerName;
+        this.executorName = executorName;
+        this.info = info;
+        this.status = Status.CREATED;
+    }
+
+    @Override
+    public String toString() {
+        return id + "; " + taskName + "; " + ownerName + "; " + executorName + "; " + info + "; " + status.rusTitle;
+    }
+
+    // сравнение задач
+    public int compareTo(Task o) {
+        return this.getStatus().order - o.getStatus().order;
+    }
+
+    // закрытие задачи
+    public void close() {
+        status = Status.CLOSSED;
+    }
+
+    // отклонить задачу
+    public void reject() {
+        status = Status.REJECTED;
+    }
+
+}
---
+++ lesson_9/src/main/java/com/ertelecom/lesson_9/TaskService.java
@@ -0,0 +1,36 @@
+package com.ertelecom.lesson_9;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface TaskService {
+    void prepare();
+
+    boolean addTask(Task obj);
+    Task getTaskById(long id);
+
+    default void addTask(int id, String taskName, String ownerName, String executorName, String info) {
+        if (getTaskById(id) != null) {
+            throw new AlreadyExistsException("Задача с ID = " + id + " уже существует!");
+        } else if (!addTask(new Task(id, taskName, ownerName, executorName, info))) {
+            throw new ListFullException("Список задач заполнен");
+        }
+    }
+
+    void delTaskById(long id);
+    void delTaskByName(String name);
+    void print();
+    void closeTaskByID(long id);
+    void rejectTaskByID(long id);
+
+
+    List<Task> getTaskByStatus(Task.Status status );
+    boolean existsTaskById(long id);
+    List<Task> getSortedTaskByStatus();
+    long getCountByStatus(Task.Status status );
+
+    void saveToFile(String filename) throws IOException;
+    void loadFromFile(String filename);
+
+
+    }
